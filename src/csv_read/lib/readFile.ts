df72f95b59d57db4e6a9c02f794034b5f9e666c2
Isac Simkin <isacs@Royces-Mac-mini.local>
2019-08-08 11:12:23 -0400

    enter

--- src/csv_read/lib/readFile.ts
+++
@@ -1,9 +0,0 @@
-import { readFile as fsReadFile } from 'fs';
-export function readFile(path: string){
-    return new Promise<string>((resolve, reject) => {
-        fsReadFile(path, 'utf8', (err, data) => {
-            if(err) reject(err);
-            resolve(data);
-        })
-    })
-} 
--- src/csv_read/lib/writeFile.ts
+++
@@ -1,9 +0,0 @@
-import { writeFile as fsWriteFile } from 'fs';
-export function writeFile(path: string, data: string){
-    return new Promise<void>((resolve, reject) => {
-        fsWriteFile(path, data, (err) => {
-            if(err) reject(err);
-            resolve();
-        })
-    })
-} 
